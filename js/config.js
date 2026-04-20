fbe7ba52b3c6efdb0d4fab8b118b9c1ac9b4766e
Mich0023 <e22080737@itmerida.edu.mx>
2026-04-20 14:28:43 -0600

    feat: add configuration file with regex definitions and initial editor boilerplate code

--- js/config.js
+++ js/config.js
@@ -4,50 +4,68 @@ const REGEX_STRING = /^"[^"]*"$/;
 
 // Tipos de datos
 // tipos = ["num", "chain", "cow"]
-const TIPOS = {"num": "int", "chain": "str", "cow": "float"};
+const TIPOS = { "num": "int", "chain": "str", "cow": "float" };
 
 // Texto inicial en el editor
-const TEXTO_INICIAL = 
-`num id_contador , id_suma , id_temp
-cow id_precio , id_descuento , id_total
-chain id_nombre , id_codigo , id_mensaje
-num id_resultado , id_contador
+const TEXTO_INICIAL =
+    // `num id_contador , id_suma , id_temp
+    // cow id_precio , id_descuento , id_total
+    // chain id_nombre , id_codigo , id_mensaje
+    // num id_resultado , id_contador
 
-funcion id_calcular ( id_contador ) -> num {
-id_resultado = id_contador * 2
-return id_resultado
-}
+    // funcion id_calcular ( id_contador ) -> num {
+    // id_resultado = id_contador * 2
+    // return id_resultado
+    // }
 
-funcion id_sumar ( id_contador , id_temp ) -> num {
-id_suma = id_contador + id_temp
-return id_suma
-}
+    // funcion id_sumar ( id_contador , id_temp ) -> num {
+    // id_suma = id_contador + id_temp
+    // return id_suma
+    // }
 
-id_contador = 0
-id_precio = 99.99
-id_descuento = 15.5
+    // id_contador = 0
+    // id_precio = 99.99
+    // id_descuento = 15.5
 
-id_suma = id_contador + 10 * 2
-id_temp = id_variable_no_declarada + 5
+    // id_suma = id_contador + 10 * 2
+    // id_temp = id_variable_no_declarada + 5
 
-id_total = id_precio - id_descuento
-id_nombre = id_precio
+    // id_total = id_precio - id_descuento
+    // id_nombre = id_precio
 
-id_contador = 5
-id_suma = id_contador + 10 / 10
-id_temp = id_suma * 2 - 2
+    // id_contador = 5
+    // id_suma = id_contador + 10 / 10
+    // id_temp = id_suma * 2 - 2
 
-id_calcular ( id_contador )
-id_sumar ( id_contador , id_temp )
-id_funcion_inexistente ( )
+    // id_calcular ( id_contador )
+    // id_sumar ( id_contador , id_temp )
+    // id_funcion_inexistente ( )
 
-for id_contador < 10 && id_contador > 0 ; id_contador = id_contador + 1 {
-id_total = id_total + id_precio * 2
-}
+    // for id_contador < 10 && id_contador > 0 ; id_contador = id_contador + 1 {
+    // id_total = id_total + id_precio * 2
+    // }
 
-for id_suma >= 5 && id_x <= 20 ; id_suma = id_suma + 1 {
-id_temp = id_temp - 1
-}
+    // for id_suma >= 5 && id_x <= 20 ; id_suma = id_suma + 1 {
+    // id_temp = id_temp - 1
+    // }
 
-id_mensaje = "Compilacion completa"
-`;
+    // id_mensaje = "Compilacion completa"
+    // `;
+    `num id_num1 id_num2 id_num3
+    chain id_chain1 id_chain2 id_chain3
+    real id_real1 id_real2 id_real3
+    real id_operacion(){
+    real id_resultado1, id_resultado2, id_resultadoFinal;
+    id_resultado1 = id_num1 + id_num2 * id_num3 - (id_num1 / id_num3);
+    id_resultado2 = (id_real1 + id_real2)+ id_real3 - id_real2 / id_real1;
+    id_resultadoFinal = id_resultado1 + id_resultado2;
+    return id_resultadoFinal; 
+    }
+    for (num id_num4 = 1; id_num4< 10 && id_num4 != 5; id_num4 ++)
+    {
+    id_num1 = id_num1 + id_num4 * 2;
+    id_real1 = (id_real1 + id_real2)/ id_real3;
+    }
+    Real Id_real4
+    Id_real4 = id_operacion()
+    `;
